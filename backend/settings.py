3a180c2f4e1626e8d1cf464fd8d55282bea741ed
jortal10 <j_ortal@hotmail.com>
2024-11-29 18:05:55 +0100

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -41,7 +41,7 @@ class _UiSettings(BaseSettings):
         env_ignore_empty=True
     )
 
-    title: str = "Observatorio Mundial de Salud y Bienestar - IA Generativaa"
+    title: str = "Observatorio Mundial de Salud y Bienestar - IA Generativa"
     logo: Optional[str] = None
     chat_logo: Optional[str] = None
     chat_title: str = "Start chatting"
